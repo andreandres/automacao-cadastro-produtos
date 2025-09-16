ebc09f81842d5cf545246361cb91170b9cfc4823
Andre Soares <andresoaressdev@gmail.com>
2025-09-16 17:47:54 -0300

    add comentarios

--- pegarposicaomouse.py
+++ pegarposicaomouse.py
@@ -2,3 +2,6 @@ import pyautogui
 from time import sleep
 sleep(3)
 print(pyautogui.position())
+#Programa para auxiliar na posicao do mouse
+
+#Rode o programa e coloque o mouse aonde ele tem que clicar, pegue o x e o y que vao aparecer no terminal e cole em pyautogui.click(x, y) no programa principal
